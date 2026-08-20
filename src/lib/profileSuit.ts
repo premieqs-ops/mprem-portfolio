c053eb38840b4828528ae94049bf1297f917e6da
premieqs-ops <premieqs@gmail.com>
2026-08-20 17:25:06 +0530

    Add profileSuit aggregator

---
+++ src/lib/profileSuit.ts
@@ -0,0 +1,6 @@
+import { profileSuitPart0 } from "@/lib/profileSuitPart0";
+import { profileSuitPart1 } from "@/lib/profileSuitPart1";
+import { profileSuitPart2 } from "@/lib/profileSuitPart2";
+import { profileSuitPart3 } from "@/lib/profileSuitPart3";
+import { profileSuitPart4 } from "@/lib/profileSuitPart4";
+export const PROFILE_SUIT = "data:image/webp;base64," + profileSuitPart0 + profileSuitPart1 + profileSuitPart2 + profileSuitPart3 + profileSuitPart4;
